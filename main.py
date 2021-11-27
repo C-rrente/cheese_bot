b393d234f7b572071019f35f5c3507aac63f0006
Cxncrete <71531248+Cxncrete@users.noreply.github.com>
2021-11-27 08:32:38 -0800

    Update
    Added extra features to the bot

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import discord
 import os
 import requests
 import json
-#import pokepy
 import random
 from keepalive import keep_alive
-#from mal import Anime
 
 client = discord.Client()
+atw = 'AROUND THE WORLD'
+
+#async def time_check():
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -21,12 +22,13 @@ def get_quote():
 #  return(poke.name)
         
 #def get_anime():
-#  anime = Anime(random.randint(1,75))
-#  return anime
+  #anime = Anime(random.randint(1,75))
+  #return anime
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
+  await client.change_presence(activity=discord.Game(name='Parmesan'))
 
 @client.event
 async def on_message(message):
@@ -65,7 +67,7 @@ async def on_message(message):
     await message.channel.send('Sup Jay')
 
   if message.content.startswith('!jed'):
-    await message.channel.send('Hey Jed')
+    await message.channel.send('Hey Jed https://open.spotify.com/track/6JnufVNLIO5F5Lk4sEVLeI?si=6de203e6f78947f4')
 
   if message.content.startswith('!cole'):
     await message.channel.send('Hallo Cole')
@@ -76,15 +78,56 @@ async def on_message(message):
   if message.content.startswith('!log'):
     await message.channel.send('https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb')
 
-#For in sentence random replies  
-  if "not pog" in message.content:
-    await message.channel.send('I won\'t pog to that')
-  elif "pog" in message.content or "Pog" in message.content:
-    await message.channel.send('I\'ll pog to that')
+  if message.content.startswith('!devin'):
+    await message.channel.send('CRANIUM https://open.spotify.com/track/2y8PnhXwnyHTxe72TtY58e?si=a08f815b12e74ce0')
+    
+  if message.content.startswith('!gg'):
+    await message.channel.send('Nice one dumbass', tts=True)
 
-  if "cheese" in message.content or "Cheese" in message.content:
+  if message.content.startswith('--version'):
+    await message.channel.send('Current Version: 1.0')
+    await message.channel.send('Updates are just organized for my OCD now')
+
+
+
+#For in sentence random replies
+  if "NOT POG" in message.content.upper():
+    await message.channel.send('I won\'t pog to that')
+    await message.add_reaction(":not_pog:811762371579740171")
+  elif "POG" in message.content.upper():
+    await message.channel.send('I\'ll pog to that')
+    await message.add_reaction(":pog:811762317314359346")
+  
+  if "CHEESE" in message.content.upper():
     await message.add_reaction('❣️')
     await message.add_reaction('🧀')
+  
+  if "AROUND THE WORLD" in message.content.upper():
+    await message.channel.send('plɹoʍ ǝɥʇ punoɹɐ')
+  
+  if "HAPPY BIRTHDAY" in message.content.upper():
+    await message.channel.send('Happy birthday! 🎊')
+  
+  if "HELP" in message.content.upper():
+    await message.channel.send('I\'m here to help!')
+  
+  if "ROLL" in message.content.upper():
+    if "D100" in message.content.upper():
+      dice = random.randint(1,100)
+      await message.channel.send(dice)
+    elif "D6" in message.content.upper():
+      dice = random.randint(1,6)
+      await message.channel.send(dice)
+    elif "D10" in message.content.upper():
+      dice = random.randint(1,10)
+      await message.channel.send(dice)
+    elif "D20" in message.content.upper():
+      dice = random.randint(1,20)
+      await message.channel.send(dice)
+    elif "D4" in message.content.upper():
+      dice = random.randint(1,4)
+      await message.channel.send(dice)
+
 
 
 #The actual api using requests
@@ -95,19 +138,13 @@ async def on_message(message):
 #A chance of Gods
   if message:
     godlike = random.randint(1,10000)
+    print(godlike)
     if godlike == 5000:
       await message.channel.send('Godlike')
-
-
-#  if message.content.startswith('!anime'):
-#    anime = get_anime()
-#    await message.channel.send(anime.title)
-#    await message.channel.send('Rating: ')
-#    await message.channel.send(anime.score)
   
-#  if message.content.startswith('!poke'):
-#    poke = pokemon()
-#    await message.channel.send(poke)
+#Hopefully a game
+  if message.content.startswith('!game'):
+    await message.channel.send('Let\'s go! Choose your weapon')
 
 keep_alive()
 client.run(os.getenv('KEY'))  
